33d9913f1f63d28db78f2d77753ab97108629433
Louka Gamal <loukagamal49@gmail.com>
2023-08-12 01:22:48 +0300

    apikey

--- src/app/core/interceptoors/auth-interceptor/auth.interceptor.ts
+++ src/app/core/interceptoors/auth-interceptor/auth.interceptor.ts
@@ -20,7 +20,7 @@ export class authInterCeptor implements HttpInterceptor {
     request = request.clone({
       url: url,
       setHeaders: {
-        apikey: `H0I3bODSbj3j5eEDYRg4Q4oEP1MeGkXg888`,
+        apikey: `H0I3bODSbj3j5eEDYRg4Q4oEP1MeGkXg`,
       },
     });
 
